aebb1fc33b5d1573cb4110ba90d310a09e6f2cca
Chinsu <lbthienan89@gmail.com>
2025-12-08 10:10:28 +0700

    chore: reset project

--- src/JumpTaskAutomation/Backend/AutomationModels.cs
+++
@@ -1,5 +0,0 @@
-namespace JumpTaskAutomation.Backend;
-
-public record AutomationResult(bool Success, string? Notes = null, TimeSpan? Duration = null);
-
-public record AutomationJob(string Name, Func<CancellationToken, Task<AutomationResult>> ExecuteAsync);
