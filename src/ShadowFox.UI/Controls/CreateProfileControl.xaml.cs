6d0f1f384033112544038f4cf95eb17d8302cf83
Chinsu <lbthienan89@gmail.com>
2025-12-09 22:18:25 +0700

    refactor: move create profile form to control and tidy combos

---
+++ src/ShadowFox.UI/Controls/CreateProfileControl.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace ShadowFox.UI.Controls;
+
+public partial class CreateProfileControl : UserControl
+{
+    public CreateProfileControl()
+    {
+        InitializeComponent();
+    }
+}
